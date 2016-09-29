6f18f5ea2ff2c29400fd4ecbb96006fb5060dfe2
Aleko Spoonamore <aspoonamore93@gmail.com>
2016-09-28 19:23:40 -0500

    New Change Test

--- UncleJoesPizza/src/com/usaa/temppackage/TestClass.java
+++ UncleJoesPizza/src/com/usaa/temppackage/TestClass.java
@@ -3,6 +3,7 @@ package com.usaa.temppackage;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		
+		System.out.println("Hello Change!");
+
 	}
 }
